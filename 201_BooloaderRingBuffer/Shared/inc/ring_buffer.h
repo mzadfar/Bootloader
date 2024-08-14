0f91281f5afdaf35e1150fee9bc68764c6482886
ahasanzadeh <hasanzadeh.amin@gmail.com>
2024-08-14 10:39:27 -0700

    Minnor modifications

--- 201_BooloaderRingBuffer/Shared/inc/ring_buffer.h
+++ 201_BooloaderRingBuffer/Shared/inc/ring_buffer.h
@@ -4,22 +4,24 @@
 
 #include "main.h"
 
-/*For max 10ms latency for 115200 bytes per second: 115200 * 10ms = 115.2 ~ 128*/
+/*For max 10ms latency for 115200 bits per second (11520 bytes (including
+ * start/stop bits) per second): 115200 * 10ms = 115.2 ~ 128*/
 #define RING_BUFFER_SIZE (128)
 
 /* Struct ------------------------------------------------------------------*/
-typedef struct ringBuffer_t
-{
-    uint8_t* buffer;
-    uint32_t mask;
-    uint32_t readIndex;
-    uint32_t writeIndex;
+typedef struct ringBuffer_t {
+  uint8_t *buffer;
+  uint32_t mask;
+  uint32_t readIndex;
+  uint32_t writeIndex;
 } ringBuffer_t;
 
-/* Prototypes ------------------------------------------------------------------*/
-void Ring_Buffer_Setup(ringBuffer_t* ringBuffer, uint8_t* buffer, uint32_t size);
-bool Ring_Buffer_Empty(ringBuffer_t* ringBuffer);
-bool Ring_Buffer_Write(ringBuffer_t* ringBuffer, uint8_t byte);
-bool Ring_Buffer_Read(ringBuffer_t* ringBuffer, uint8_t* byte);
+/* Prototypes
+ * ------------------------------------------------------------------*/
+void Ring_Buffer_Setup(ringBuffer_t *ringBuffer, uint8_t *buffer,
+                       uint32_t size);
+bool Ring_Buffer_Empty(ringBuffer_t *ringBuffer);
+bool Ring_Buffer_Write(ringBuffer_t *ringBuffer, uint8_t byte);
+bool Ring_Buffer_Read(ringBuffer_t *ringBuffer, uint8_t *byte);
 
 #endif /* __RING_BUFFER_H */
--- 201_BooloaderRingBuffer/Shared/src/ring_buffer.c
+++ 201_BooloaderRingBuffer/Shared/src/ring_buffer.c
@@ -1,49 +1,44 @@
 #include "ring_buffer.h"
 
-void Ring_Buffer_Setup(ringBuffer_t* ringBuffer, uint8_t* buffer, uint32_t size)
-{
-    ringBuffer->buffer = buffer;
-    ringBuffer->readIndex = 0;
-    ringBuffer->writeIndex = 0;
-    ringBuffer->mask = size - 1;
+void Ring_Buffer_Setup(ringBuffer_t *ringBuffer, uint8_t *buffer,
+                       uint32_t size) {
+  ringBuffer->buffer = buffer;
+  ringBuffer->readIndex = 0;
+  ringBuffer->writeIndex = 0;
+  ringBuffer->mask = size - 1; // size is a power of 2
 }
 
-bool Ring_Buffer_Empty(ringBuffer_t* ringBuffer)
-{
-    return ringBuffer->readIndex == ringBuffer->writeIndex;
+bool Ring_Buffer_Empty(ringBuffer_t *ringBuffer) {
+  return ringBuffer->readIndex == ringBuffer->writeIndex;
 }
 
-bool Ring_Buffer_Write(ringBuffer_t* ringBuffer, uint8_t byte)
-{
-    uint32_t localReadIndex = ringBuffer->readIndex;
-    uint32_t localWriteIndex = ringBuffer->writeIndex;
+bool Ring_Buffer_Write(ringBuffer_t *ringBuffer, uint8_t byte) {
+  uint32_t localReadIndex = ringBuffer->readIndex;
+  uint32_t localWriteIndex = ringBuffer->writeIndex;
 
-    uint32_t nextWriteIndex = (localWriteIndex + 1) & ringBuffer->mask;
+  uint32_t nextWriteIndex = (localWriteIndex + 1) & ringBuffer->mask;
 
-    if (nextWriteIndex == localReadIndex)
-    {
-        return false;
-    }
+  if (nextWriteIndex == localReadIndex) {
+    return false; // we write on not-used/read index
+  }
 
-    ringBuffer->buffer[localWriteIndex] = byte;
-    ringBuffer->writeIndex = nextWriteIndex;
+  ringBuffer->buffer[localWriteIndex] = byte;
+  ringBuffer->writeIndex = nextWriteIndex;
 
-    return true;
+  return true;
 }
 
-bool Ring_Buffer_Read(ringBuffer_t* ringBuffer, uint8_t* byte)
-{
-    uint32_t localReadIndex = ringBuffer->readIndex;
-    uint32_t localWriteIndex = ringBuffer->writeIndex;
+bool Ring_Buffer_Read(ringBuffer_t *ringBuffer, uint8_t *byte) {
+  uint32_t localReadIndex = ringBuffer->readIndex;
+  uint32_t localWriteIndex = ringBuffer->writeIndex;
 
-    if (localWriteIndex == localReadIndex)
-    {
-        return false;
-    }
+  if (localWriteIndex == localReadIndex) {
+    return false;
+  }
 
-    *byte = ringBuffer->buffer[localReadIndex];
-    localReadIndex = (localReadIndex + 1) & ringBuffer->mask;
-    ringBuffer->readIndex = localReadIndex;
+  *byte = ringBuffer->buffer[localReadIndex];
+  localReadIndex = (localReadIndex + 1) & ringBuffer->mask;
+  ringBuffer->readIndex = localReadIndex;
 
-    return true;
+  return true;
 }
